ad72ac9eff8b147156410f1018ca5af9b3b71aa6
rallardm@gmail.com <rallardm@gmail.com>
2024-02-09 18:32:22 -0500

    three states unstable

--- Assets/Scripts/FSM/ZombieStates/ChasingState.cs
+++ Assets/Scripts/FSM/ZombieStates/ChasingState.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ChasingState : ZombieState
 {
+
     public override bool CanEnter(IState currentState)
     {
         return m_stateMachine.m_isPreyInSight;
@@ -19,24 +20,15 @@ public class ChasingState : ZombieState
 
     public override void OnExit()
     {
+        m_stateMachine.ZombieAnimator.SetBool("IsRunning", false);
         Debug.Log("Exiting Chasing State");
     }
 
     public override void OnFixedUpdate()
     {
-        Debug.Log("Chasing State OnFixedUpdate");
-        if (m_stateMachine.RB == null) Debug.Log("RB is null");
-        if (m_stateMachine.m_preyPosition == null) Debug.Log("Prey position is null");
-
-        var direction = m_stateMachine.m_preyPosition - m_stateMachine.RB.transform.position;
-        // Rotate horizontally towards the prey
-        var rotation = Quaternion.LookRotation(direction);
-        Quaternion horizintalRotation = Quaternion.Euler(0, rotation.eulerAngles.y, 0);
-        m_stateMachine.RB.transform.rotation = Quaternion.Slerp(m_stateMachine.RB.transform.rotation, horizintalRotation, Time.deltaTime * 2.0f);
-
-        // Advance towards the prey
-        var vectorOnFloor = new Vector3(direction.x, 0, direction.z);
-        m_stateMachine.RB.AddForce(vectorOnFloor * 100f, ForceMode.Acceleration);
+        m_stateMachine.ZombieAnimator.SetBool("IsWalking", false);
+        m_stateMachine.ZombieAnimator.SetBool("IsRunning", true);
+        m_stateMachine.GoToDirection(m_stateMachine.m_preyPosition);
     }
 
     public override void OnStart()
@@ -46,6 +38,6 @@ public class ChasingState : ZombieState
 
     public override void OnUpdate()
     {
-        Debug.Log("Chasing State OnUpdate");
+        //Debug.Log("Chasing State OnUpdate");
     }
 }
--- Assets/Scripts/FSM/ZombieStates/RoamingState.cs
+++ Assets/Scripts/FSM/ZombieStates/RoamingState.cs
@@ -4,29 +4,42 @@ using UnityEngine;
 
 public class RoamingState : ZombieState
 {
+    private float m_roamingCountdown = 16f;
+    private float m_newDirectionCountdown = 8f;
+    
+
     public override bool CanEnter(IState currentState)
     {
-        return true;
+        return !m_stateMachine.m_isPreyInSight;
     }
 
     public override bool CanExit()
     {
-        return true;
+        return m_roamingCountdown <= 0f;
     }
 
     public override void OnEnter()
     {
+        m_stateMachine.ZombieAnimator.SetBool("IsRunning", false);
+        m_stateMachine.ZombieAnimator.SetBool("IsWalking", true);
         Debug.Log("Entering Roaming State");
     }
 
     public override void OnExit()
     {
         Debug.Log("Exiting Roaming State");
+        m_roamingCountdown = 16f;
     }
 
     public override void OnFixedUpdate()
     {
         //Debug.Log("Roaming State OnFixedUpdate");
+        if (m_newDirectionCountdown <= 0f)
+        {
+            m_newDirectionCountdown = 8f;
+            m_stateMachine.m_newDirection = new Vector3(Random.Range(-30f, 30f), Random.Range(-30f, 30f), Random.Range(-30f, 30f));
+            m_stateMachine.GoToDirection(m_stateMachine.m_newDirection);
+        }
     }
 
     public override void OnStart()
@@ -37,5 +50,9 @@ public class RoamingState : ZombieState
     public override void OnUpdate()
     {
         //Debug.Log("Roaming State OnUpdate");
+        m_roamingCountdown -= Time.deltaTime;
+        m_newDirectionCountdown -= Time.deltaTime;
+
+
     }
 }
